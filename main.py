6d138594ef83ca77a19d1e4fef8bb55bd3a0afcb
elleferns <ellyferns@outlook.com>
2026-06-19 15:23:53 +0100

    3: added player body and rotation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -12,16 +13,18 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        screen.fill("black")
-        pygame.display.flip()
         dt = clock.tick(60) / 1000
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
